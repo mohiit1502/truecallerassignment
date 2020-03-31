02ecfd315e60803a85c5ea5f88006d5394b49cf5
vaasu <mohiit1502@gmail.com>
2020-04-01 03:09:05 +0530

    First commit

---
+++ src/test/java/com/truecaller/truecallerassignment/backend/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truecaller.truecallerassignment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruecallerAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
