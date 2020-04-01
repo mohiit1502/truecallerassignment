7d45f8a30f731fa4d8eb6253913564317b60cf3e
vaasu <mohiit1502@gmail.com>
2020-04-02 01:04:36 +0530

    Fixed import of crossorigin error

--- src/main/java/com/truecaller/truecallerassignment/backend/controller/TruecallerAssignmentController.java
+++ src/main/java/com/truecaller/truecallerassignment/backend/controller/TruecallerAssignmentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.truecaller.truecallerassignment.backend.entities.CBRouteResponse;
 import com.truecaller.truecallerassignment.backend.entities.Tile;
